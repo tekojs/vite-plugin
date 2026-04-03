7dd6396ca2f73f35d76b174aeff6a4123102c193
JefteCosta <jefte@meetrox.ai>
2026-04-03 15:40:49 -0300

    feat(lint): add lint e prettie para dadronizar o projeto

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    ignores: ["dist/**", "node_modules/**"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  eslintConfigPrettier,
+];
