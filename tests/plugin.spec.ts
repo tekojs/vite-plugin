08703ece9c84517d86a2bb8738b63a22dfc0e617
JefteCosta <jefte@meetrox.ai>
2026-04-05 13:43:15 -0300

    feat(test): modelo de teste

--- tests/plugin.spec.ts
+++ tests/plugin.spec.ts
@@ -2,6 +2,6 @@ import { test } from '@t8ngs/runner'
 
 test.group('Plugin tests', () => {
   test('should run the plugin', async ({ assert }) => {
-    assert(true).toBe(true)
+    assert.isTrue(true)
   })
 })
